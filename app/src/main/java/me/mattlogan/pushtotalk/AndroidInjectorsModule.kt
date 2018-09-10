8dc8694bc0ea8cda15b1617230f67363411a389a
Matt Logan <matt.b.logan@gmail.com>
2018-09-09 18:12:07 -0700

    Imports cleanup

--- app/src/main/java/me/mattlogan/pushtotalk/AndroidInjectorsModule.kt
+++ app/src/main/java/me/mattlogan/pushtotalk/AndroidInjectorsModule.kt
@@ -2,11 +2,9 @@ package me.mattlogan.pushtotalk
 
 import dagger.Module
 import dagger.android.ContributesAndroidInjector
-import me.mattlogan.pushtotalk.audio.AudioRecorderModule
 import me.mattlogan.sendmessage.SendMessageFragment
 import me.mattlogan.sendmessage.SendMessageModule
 import me.mattlogan.uicommon.ViewModelFactoryModule
-import me.mattlogan.upload.UploadClientModule
 
 @Module(includes = [
   ViewModelFactoryModule::class
