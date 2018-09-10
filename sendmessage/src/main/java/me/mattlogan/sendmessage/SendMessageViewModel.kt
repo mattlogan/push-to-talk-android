b70aee5bf21779376eece5f1b13d0649bfba92eb
Matt Logan <matt.b.logan@gmail.com>
2018-09-09 17:43:18 -0700

    Add toLiveData() extension

--- sendmessage/src/main/java/me/mattlogan/sendmessage/SendMessageViewModel.kt
+++ sendmessage/src/main/java/me/mattlogan/sendmessage/SendMessageViewModel.kt
@@ -1,10 +1,9 @@
 package me.mattlogan.sendmessage
 
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.LiveDataReactiveStreams
 import androidx.lifecycle.ViewModel
-import io.reactivex.BackpressureStrategy
 import io.reactivex.Observable
+import me.mattlogan.uicommon.toLiveData
 import javax.inject.Inject
 
 class SendMessageViewModel @Inject constructor(
@@ -17,13 +16,11 @@ class SendMessageViewModel @Inject constructor(
   val state: LiveData<SendMessageUpdate>
 
   init {
-    state = LiveDataReactiveStreams.fromPublisher(
-        Observable.merge(
-            uiEvents.startRecording
-                .compose(startRecordingTransformer),
-            uiEvents.stopRecording
-                .compose(stopRecordingTransformer)
-        ).toFlowable(BackpressureStrategy.LATEST)
-    )
+    state = Observable.merge(
+        uiEvents.startRecording
+            .compose(startRecordingTransformer),
+        uiEvents.stopRecording
+            .compose(stopRecordingTransformer)
+    ).toLiveData()
   }
 }
---
+++ uicommon/src/main/java/me/mattlogan/uicommon/LiveDataExtensions.kt
@@ -0,0 +1,10 @@
+package me.mattlogan.uicommon
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.LiveDataReactiveStreams
+import io.reactivex.BackpressureStrategy
+import io.reactivex.Observable
+
+fun <T> Observable<T>.toLiveData(): LiveData<T> {
+  return LiveDataReactiveStreams.fromPublisher(this.toFlowable(BackpressureStrategy.LATEST))
+}
